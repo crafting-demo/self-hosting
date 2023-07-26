c94db3d42357bc0a0c180fbd3b02e726563ebf64
Yuan Zhuang <whywhy36@gmail.com>
2023-07-26 19:49:56 +1200

    add openssh support

--- apps/aws-windowsvm/tf/main.tf
+++ apps/aws-windowsvm/tf/main.tf
@@ -28,4 +28,29 @@ resource "aws_instance" "vm" {
     name = var.launch_template_name
   }
   get_password_data      = true
+  user_data = <<EOF
+<powershell>
+# Install the OpenSSH Client
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+
+# Install the OpenSSH Server
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+
+# Start the sshd service
+Start-Service sshd
+
+# OPTIONAL but recommended:
+Set-Service -Name sshd -StartupType 'Automatic'
+
+# Confirm the Firewall rule is configured. It should be created automatically by setup. Run the following to verify
+if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyContinue | Select-Object Name, Enabled)) {
+    Write-Output "Firewall Rule 'OpenSSH-Server-In-TCP' does not exist, creating it..."
+    New-NetFirewallRule -Name 'OpenSSH-Server-In-TCP' -DisplayName 'OpenSSH Server (sshd)' -Enabled True -Direction Inbound -Protocol TCP -Action Allow -LocalPort 22
+} else {
+    Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
+}
+
+</powershell>
+EOF
+
 }
